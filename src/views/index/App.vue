5d1e117661ff88ee09bd9eee8d4919b25beb4b26
am-zxm <zxm0825@qq.com>
2023-04-14 00:04:39 +0800

    hello

--- src/views/index/App.vue
+++ src/views/index/App.vue
@@ -10,7 +10,6 @@ export default {
     // 取消开始的loading动画
     const preLoader = document.querySelector('#pre-loader')
     preLoader.style.display = 'none'
-    alert('Page has been loaded') // alert() can take any message string as argument. It will be displayed as a message box.
 
     // fix: firefox 下 拖拽 会新打卡一个选项卡
     // https://github.com/JakHuang/form-generator/issues/15
