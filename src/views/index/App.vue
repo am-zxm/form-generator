2de0bb599057b8efd9bdfbd4ed1fa535f1c404a7
am-zxm <zxm0825@qq.com>
2023-04-14 00:01:09 +0800

    还原

--- src/views/index/App.vue
+++ src/views/index/App.vue
@@ -10,9 +10,8 @@ export default {
     // 取消开始的loading动画
     const preLoader = document.querySelector('#pre-loader')
     preLoader.style.display = 'none'
-    alert()
 
-    // fix: firefox 下 拖拽 会新打卡一个选项卡220334
+    // fix: firefox 下 拖拽 会新打卡一个选项卡
     // https://github.com/JakHuang/form-generator/issues/15
     document.body.ondrop = event => {
       event.preventDefault()
